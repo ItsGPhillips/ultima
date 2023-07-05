8fbe1b9ee8a030a4181e2dfb01173c70f41460b6
George Phillips <112098195+ItsGPhillips@users.noreply.github.com>
2023-07-05 23:36:33 +0100

    user.signout route implimentation

--- apps/website/src/app/api/auth/[endpoint]/user.signout.ts
+++ apps/website/src/app/api/auth/[endpoint]/user.signout.ts
@@ -1,5 +1,23 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { auth } from "@website/lucia";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-export const signOutUser = async (req: NextRequest): Promise<Response> => {
-  return NextResponse.next();
+export const signOutUser = async (req: NextRequest): Promise<NextResponse> => {
+   const authRequest = auth.handleRequest({ request: req, cookies });
+   const { session } = await authRequest.validateUser();
+   if (!session) {
+      return NextResponse.json(null, {
+         status: 401,
+      });
+   }
+
+   await auth.invalidateSession(session.sessionId);
+   authRequest.setSession(null); // delete session cookie
+
+   return NextResponse.json(null, {
+      status: 302,
+      headers: {
+         location: "/",
+      },
+   });
 };
