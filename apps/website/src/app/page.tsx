a100c2bf99dc13aa50178e2a8b1c3eca7e7ee2b0
George Phillips <112098195+ItsGPhillips@users.noreply.github.com>
2023-07-06 17:43:03 +0100

    trpc boilerplate

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,5 +1,9 @@
-const Page = () => {
-   return null;
+import { appRouter } from "@website/api"
+
+const Page = async () => {
+   const caller = appRouter.createCaller({})
+   const greeting = await caller.greeting.getGreeting();
+   return <>{greeting.message}</>
 };
 
 export default Page;
